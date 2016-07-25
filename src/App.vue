f9b3621e797256fb2cc077c39e3c8ceff4414037
张卓 <contrary.zhuo@gmail.com>
2016-07-25 22:24:52 +0800

    [init] get camera video

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,62 @@
 <template>
   <div id="app">
+    <video :src="videoSrc" autoplay class="video" v-el:video></video>
   </div>
 </template>
 
 <script>
 export default {
+  async ready () {
+    await this.getCameraIDs()
+    this.getVideo()
+  },
+  data () {
+    return {
+      cameraIds: [],
+      currentCamera: 1,
+      videoSrc: ''
+    }
+  },
+  methods: {
+    getCameraIDs () {
+      MediaStreamTrack.getSources((sources) => {
+        sources.forEach(source => {
+          console.log(source)
+          if (source.kind === 'video') {
+            this.cameraIds.push(source.id)
+          }
+        })
+      })
+    },
+    getVideo () {
+      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
+      navigator.getUserMedia({
+        video: {
+          optional: [{sourceId: this.cameraIds[this.currentCamera]}]
+        }
+      }, localMediaStream => {
+        this.videoSrc = window.URL.createObjectURL(localMediaStream)
+      }, err => {
+        console.log(err)
+      })
+    }
+  },
   components: {
-    Hello
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+.video {
+  position: fixed;
+  z-index: -1;
+  width: 100%;
+  height: 100vh;
+  object-fit: cover;
+}
 </style>
