74124a572eee7a25e6257c75e7f044a4855f8b2f
张卓 <contrary.zhuo@gmail.com>
2016-07-27 23:45:27 +0800

    [update] add shoot

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
     <video :src="videoSrc" autoplay class="video" v-el:video></video>
+    <div class="toolbox__bottom">
+      <button class="shoot--button" @click="shoot"></button>
+    </div>
   </div>
 </template>
 
 <script lang="babel">
 import { getCameraVideoSrc } from './services/camera'
+import { saveFile, getFile, getAllFiles } from './services/file'
 export default {
   async ready () {
     this.videoSrc = await getCameraVideoSrc()
@@ -16,6 +20,32 @@ export default {
     }
   },
   methods: {
+    shoot () {
+      const canvas = document.createElement('canvas')
+      canvas.width = this.$els.video.videoWidth
+      canvas.height = this.$els.video.videoHeight
+      const ctx = canvas.getContext('2d')
+      ctx.drawImage(this.$els.video, 0, 0)
+      canvas.toBlob(blob => {
+        saveFile(blob).then(e => {
+          console.log(e)
+        }).catch(err => {
+          console.log(err)
+        })
+      }, 'image/jpeg')
+    },
+    getAllImages () {
+      getAllFiles().then(files => {
+        files.forEach(file => {
+          getFile(file.fullPath).then(result => {
+            console.log(result)
+          }).catch(err => {
+            console.log(err)
+          })
+        })
+      }).catch(err => {
+        console.log(err)
+      })
     }
   },
   components: {
@@ -35,5 +65,26 @@ body {
   width: 100%;
   height: 100vh;
   object-fit: cover;
+}
+.toolbox__bottom {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 16vh;
+  background-color: rgba(255, 255, 255, .6);
+  backdrop-filter: blur(25px);
+}
+.shoot--button {
+  position: fixed;
+  bottom: 25px;
+  left: 50%;
+  z-index: 9999;
+  transform: translateX(-50%);
+  width: 60px;
+  height: 60px;
+  background-color: rgba(255, 0, 0, .6);
+  border: none;
+  border-radius: 50%;
+  outline: none;
 }
 </style>
