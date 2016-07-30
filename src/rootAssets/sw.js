6508dcac9c4b4454b39eb6b607b67481f4163291
张卓 <contrary.zhuo@gmail.com>
2016-07-31 01:29:22 +0800

    Update sw.js

--- src/rootAssets/sw.js
+++ src/rootAssets/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('airhorner').then(cache => {
+    caches.open('hiraeth').then(cache => {
       return cache.addAll([
         '/',
         '/index.html',
