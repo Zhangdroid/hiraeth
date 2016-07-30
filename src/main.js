ff02c39af1548dffca4ba8435dd1926e113e7708
张卓 <contrary.zhuo@gmail.com>
2016-07-30 22:36:53 +0800

    [add] vue-async-computed

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import 'webrtc-adapter'
+import AsyncComputed from 'vue-async-computed'
 import Vue from 'vue'
 import App from './App'
+
+Vue.use(AsyncComputed)
 
 /* eslint-disable no-new */
 new Vue({
